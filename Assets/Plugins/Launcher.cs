5b5ecd65598c84f3f7112fa5d3911d08290aa755
absolsonga <absolsonga@naver.com>
2016-09-25 16:22:21 +0900

    Make Some Scenes and Scripts

---
+++ Assets/Plugins/Launcher.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+using DG.Tweening;
+
+public class Launcher : MonoBehaviour
+{
+    public GameObject Missile; // 미사일 프리펩
+
+	void Start()
+	{
+        DOTween.Init(false, true, LogBehaviour.ErrorsOnly); // DotTween 초기화
+        transform.DOMoveX(5f, 4f).SetLoops(-1, LoopType.Yoyo).SetEase(Ease.Linear); // 런쳐를 좌우로 반복 이동시킨다.
+    }
+
+    public void Shoot()
+    {
+        // 한번 쏠때마다 유도탄을 4개씩 생성한다.
+        Instantiate(Missile, transform.GetChild(0).position, Quaternion.Euler(0f, 0f, 70f));
+        Instantiate(Missile, transform.GetChild(0).position, Quaternion.Euler(0f, 0f, -70f));
+
+        Instantiate(Missile, transform.GetChild(0).position, Quaternion.Euler(0f, 0f, 40f));
+        Instantiate(Missile, transform.GetChild(0).position, Quaternion.Euler(0f, 0f, -40f));
+    }
+}
---
+++ Assets/Plugins/Target.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+using DG.Tweening;
+
+public class Target : MonoBehaviour
+{
+	void Start()
+	{
+        // 타겟을 좌우로 반복 이동한다. 시간을 랜덤하게 해서 서로 다른 속도로 설정한다.
+        transform.DOMoveX(transform.position.x + Random.Range(4f, 6f), Random.Range(1f, 2f)).SetLoops(-1, LoopType.Yoyo).SetEase(Ease.Linear);
+    }
+}
